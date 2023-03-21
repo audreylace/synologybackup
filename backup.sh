5986bbebbeaf4218874c843845a506f3cf54a4b0
Audrey Lace <audrey.lace.taylor@gmail.com>
2023-03-21 14:26:30 -0500

    file structure

---
+++ backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Need this to support crontab
+cd "$(dirname "$0")"
+
+NOW=$(date '+%s')
+SRCPATH=
+USERNAME=
+BACKUPSERVER=
+DSTPATH=
+
+# Support for systems that hibernate
+#
+#LASTRUN=$(date -r './last-backup' '+%s')
+#DIFF=$(expr $NOW - $LASTRUN)
+#
+#
+#if ((DIFF > 3600)); then
+#echo 1 > "./last-backup"
+
+TODAY=$( date '+%Y.%m.%d.%H.%M.%S')
+rsync -aq --log-file="./logs/backup-$TODAY.log" --delete --exclude-from="./backup-exclude-list" -e "sshpass -f './password' ssh" $SRCPATH $USERNAME@$BACKUPSERVER:$DSTPATH
+
+
+# Log rotation support
+#
+LASTROTATE=$(date -r './last-rotate' '+%s')
+
+# Do rotation every 7 days
+#
+if (($(expr $NOW - $LASTROTATE) > 604800)); then 
+
+ echo 1 > "./last-rotate"
+ echo "Checking for logs to archive"
+
+ if find ./logs -maxdepth 1 -mtime +30 -type f -exec false {} +
+ then
+  echo "No logs to archive"
+ else
+  echo "Found logs to archive"
+  find ./logs -maxdepth 1 -mtime +30 -type f -print0 | tar -czvf ./logs/archive/oldlogs_$TODAY.tar.gz --remove-files --null -T /dev/stdin
+ fi
+else
+ echo "Too early for log rotation"
+fi
+
+#fi
